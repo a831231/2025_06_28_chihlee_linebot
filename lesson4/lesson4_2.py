67512a511761a4ea811747456832025bcf56afc9
zoeyliu <zoeyliu1231@gmail.com>
2025-07-19 16:04:13 +0800

    0719建立變數3

---
+++ lesson4/lesson4_2.py
@@ -0,0 +1,17 @@
+# list
+fruits = ["apple", "banana", "cherry" ,"orange", "kiwi", "melon", "mango"]
+fruits.append("grape") #新增元素
+fruits.append("pear") #再新增一個元素
+fruits.append("pineapple")
+fruits.append("papaya")
+fruits.append("plum")
+fruits.append("peach")
+fruits.append("strawberry")
+fruits.append("blueberry")
+fruits.append("watermelon")
+fruits.append("lemon")
+fruits.append("lime")
+fruits.append("passionfruit")
+print(type(fruits))
+print("水果清單:",fruits)
+
